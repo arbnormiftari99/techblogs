3f7331f81fc8df635de856f1361937dc59e08007
Sheriff-Hoti <sherifhoti1235@hotmail.com>
2023-02-28 23:01:09 +0100

    Instead of hardcoded values now we'll get them from an .env file located into the root folder

--- backend/index.js
+++ backend/index.js
@@ -1,35 +1,76 @@
+const blogRoute = require('./routes/blog.route');
+
 const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+// const bodyParser = require('body-parser');
+const createError = require('http-errors');
+require('dotenv').config();
+
+
 const app = express();
-const PORT = 8081;
+const port = process.env.PORT || 4000
+console.log(process.env.PORT+"-the port")
+//mongoDB connection
+mongoose
+  .connect(process.env.CONNECTION_STRING)
+  .then((x) => {
+    console.log(`Connected to Mongo! Database name: "${x.connection.name}"`)
+  })
+  .catch((err) => {
+    console.error('Error connecting to mongo', err.reason)
+  })
 
 /*applying the express json middleware 
 so that every request that hits the endpoints,
  their request bodies can be converted to json*/
 
-app.use(express.json());
+ //if it doesnt work switch to bodyparser
+app.use(express.json())
+app.use(
+    express.urlencoded({
+      extended: false,
+    }),
+)
+app.use(cors());
+
+app.use('/api/blogs', blogRoute);
+
+// app.get('/blogs',(req,res) => {
+//     res.status(200).send({
+//         hi:'hello',
+//         size:'large'
+//     })
+// })
 
 app.listen(
-    PORT,
-    () => console.log(`server running on http://localhost:${PORT}`)
+    port,
+    () => console.log(`server running on http://localhost:${port}`)
 );
 
-app.get('/blogs',(req,res) => {
-    res.status(200).send({
-        hi:'hello',
-        size:'large'
-    })
+app.use((req, res, next) => {
+    next(createError(404))
+})
+  
+  // error handler
+app.use((err, req, res) => {
+  console.error(err.message)
+  if (!err.statusCode) err.statusCode = 500
+  res.status(err.statusCode).send(err.message)
 })
 
-app.post('/blogs/:id',(req,res) => {
-    const { id } = req.params;
-    const { body } = req.body;
-    if(!body){
-        res.status(418).send({
-        message:'missing body'
-        })
-    }
-    res.status(200).send({
-        id: id,
-        body: body
-    })
-})
+
+
+// app.post('/blogs/:id',(req,res) => {
+//     const { id } = req.params;
+//     const { body } = req.body;
+//     if(!body){
+//         res.status(418).send({
+//         message:'missing body'
+//         })
+//     }
+//     res.status(200).send({
+//         id: id,
+//         body: body
+//     })
+// })
--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -1,16 +1,18 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
-
+import dotenv from 'dotenv';
+dotenv.config({path:'../../.env'})
 const firebaseConfig = {
-  apiKey: "AIzaSyBZj7roL-gjba9a_PDriKlgt1gEX4QUV_I",
-  authDomain: "techblogs-85a21.firebaseapp.com",
-  projectId: "techblogs-85a21",
-  storageBucket: "techblogs-85a21.appspot.com",
-  messagingSenderId: "1071150006471",
-  appId: "1:1071150006471:web:82b6a45e7843fab2153ee2",
-  measurementId: "G-DGBKT4KYNR"
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID,
+  measurementId: process.env.VUE_APP_MEASUREMENT_ID
 };
 
+console.log( process.env.VUE_APP_API_KEY)
 
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
